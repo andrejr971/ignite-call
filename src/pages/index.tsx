b5bb6b00e1e7f0454a28315ba8a887da6af577f7
André Junior <andrejunior179@gmail.com>
2023-07-20 20:14:02 -0300

    feat: :sparkles: add page home and components

--- src/pages/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum, possimus officia doloremque dicta rerum, odio obcaecati ratione a nihil accusamus culpa harum reiciendis rem ipsa voluptate autem natus. Quo, eum!
-    </>
-  )
-}
